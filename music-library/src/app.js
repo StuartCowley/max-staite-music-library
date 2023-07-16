1f687969a861f8f882cdad56694d09d45f9e15c5
Max Staite <max.staite2004@gmail.com>
2023-07-16 20:08:56 +0100

    creating artist

--- music-library/src/app.js
+++ music-library/src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const artistRouter = require('./routes/artist');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).json({ Result: "Hello World"})
-});
+app.use('/artists', artistRouter);
 
 
 module.exports = app;
