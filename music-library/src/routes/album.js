007d9f79ee461772370761e6919e03059fa2d038
Max Staite <max.staite2004@gmail.com>
2023-07-28 19:25:47 +0100

    album tests: read, update

--- music-library/src/routes/album.js
+++ music-library/src/routes/album.js
@@ -3,6 +3,10 @@ const albumController = require('../controllers/album');
 
 const  albumRouter = express.Router();
 
+albumRouter.get('/',albumController.readAlbums)
 
+albumRouter.get('/:id', albumController.readAlbumById)
+
+albumRouter.patch('/:id', albumController.updateAlbums)
 
 module.exports = albumRouter
